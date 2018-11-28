bc8508b9d64a7dbab0466bc464354d0766eabafc
stas <stas@vlad-2.local>
2018-11-28 22:58:07 +0300

    s

---
+++ components/TheProjects/Slides.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="slides">
+{{image.title}}
+    
+    <div class="slideImage">
+
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    props : ["image"]
+}
+</script>
+<style scoped>
+.slides{
+
+}
+.slideImage{
+    background-position: center;
+    background-size: cover;
+    background-image: url("static/1.jpg")
+}
+
+</style>
